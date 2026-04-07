150bd911da3d09865d36399a0a1f38b5b56088d6
SACHITH <kasasachith@gmail.com>
2026-04-07 23:20:22 +0530

    written the 26 program

---
+++ exp_27.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Min:", arr.min())
+print("Max:", arr.max())
+print("Sum:", arr.sum())
+print("Cumulative Sum:", arr.cumsum())
+
+# Min: 1
+# Max: 5
+# Sum: 15
+# Cumulative Sum: [ 1  3  6 10 15]

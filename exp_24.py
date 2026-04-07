a47a6f02700945264ed5b1f13b5a87e54d5c288f
SACHITH <kasasachith@gmail.com>
2026-04-07 23:17:59 +0530

    written the 24 program

---
+++ exp_24.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([[1, 2], [3, 4]])
+
+print("1D Array:", arr1)
+print("2D Array:\n", arr2)
+
+# output:
+# 1D Array: [1 2 3 4]
+# 2D Array:
+#  [[1 2]
+#  [3 4]]
+

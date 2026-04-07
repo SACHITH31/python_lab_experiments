623e1fb5b62a11e1ea5562395cccfb1ee2b0aa3a
SACHITH <kasasachith@gmail.com>
2026-04-07 23:18:55 +0530

    written the 25 program

---
+++ exp_25.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print("Dimensions:", arr.ndim)
+print("Shape:", arr.shape)
+print("Size:", arr.size)
+print("Data Type:", arr.dtype)
+
+# Dimensions: 2
+# Shape: (3, 3)
+# Size: 9
+# Data Type: int64
